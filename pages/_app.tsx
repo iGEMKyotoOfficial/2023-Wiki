66dfca6a379e0606162e15ee75ac0f12e7214fe7
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-06-11 16:23:20 +0900

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import '../styles/globals.css'
+import { MDXProvider } from '@mdx-js/react'
+import { ReactNode } from 'react'
+
+// Markdown style settings
+function H1(props: { children?: ReactNode }) {
+  return <h1 className="text-blue">{props.children}</h1>
+}
+
+const components = {
+  h1: H1
+}
+
+export default function MyApp({ Component, pageProps }:
+  { Component: any, pageProps: any }) {
+  return (
+    <MDXProvider components={components}>
+        <Component{...pageProps} />
+    </MDXProvider>
+  )
+}
