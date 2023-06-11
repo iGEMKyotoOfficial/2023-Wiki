11a0b58cb0b706ddf7eedb37a35842cef0b01769
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-06-11 18:51:44 +0900

    add react-fortawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,11 @@ import Layout from 'components/layout'
 import { MDXProvider } from '@mdx-js/react'
 import { ReactNode } from 'react'
 
+// Font Awesomeの設定
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 // Markdown style settings
 function H1(props: { children?: ReactNode }) {
   return <h1 className="text-blue">{props.children}</h1>
