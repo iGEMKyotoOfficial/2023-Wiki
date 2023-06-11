e64f931700a9934273e72dbca5205e9179fb1b0c
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-06-11 17:06:01 +0900

    Description

---
+++ pages/description.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import Container from 'components/container'
+
+export default function Description() {
+  return <>
+    <Container>
+      <Link href={"https://google.co.jp/"}>hoge</Link>
+    </Container>
+  </>
+}
