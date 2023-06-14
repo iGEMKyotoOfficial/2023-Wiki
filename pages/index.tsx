34b49a946924ee8b373943a26cbd75b91b6a4e4e
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-06-14 16:47:01 +0900

    apostrophi

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Container from 'components/container'
 export default function Home() {
   return <>
     <Container>
-      <h1>Welcome to iGEM Kyoto's Wiki</h1>
+      <h1>Welcome to iGEM Kyoto&apos;s Wiki</h1>
       <h2>Project Description</h2>
       <p style={{ margin: "2em auto" }}>
         <Link href="/description" className="btn btn-flat">
