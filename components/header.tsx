deadde4e74aab5165e2648d51af1b2ee82c8ab16
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-06-16 20:41:23 +0900

    navbar reverted (non-functional)

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 import Container from 'components/container'
 import Logo from 'components/logo'
-import Navbar from 'components/nav2'
+import Nav from 'components/nav'
 import styles from 'styles/header.module.css'
 
 export default function Header(){
@@ -9,7 +9,7 @@ export default function Header(){
         <Container large>
             <div className={styles.flexContain}>
                 <Logo />
-                <Navbar />
+                <Nav />
             </div>
         </Container>
     </header>
